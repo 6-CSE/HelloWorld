6eaa0a06023ac898701053c32eafada55ee7650f
Rishit Saiya <180010027@iitdh.ac.in>
2020-10-02 11:21:49 +0530

    Update and rename README.md to HelloWorld.cbl

---
+++ Basic Android RE 1/HelloWorld.cbl
@@ -0,0 +1,8 @@
+```cobol
+       identification division.
+       program-id. cobol.
+       procedure division.
+       main.
+           display 'Hello World.' end-display.
+           stop run.
+```
