7b83ecac2761e6695d89007a8e1519d8c8084f0e
maurxeugenio <maurxeugenio@gmail.com>
2020-10-21 11:02:53 -0300

    hello world random

---
+++ Python/hello_word.py
@@ -0,0 +1,10 @@
+from random import randint
+hw = ['o', 'l', 'w', 'l', 'e', 'h', 'l', 'r', 'd']
+
+count = 0
+while True:
+    result = f'{hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]} {hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]}'
+    print(result)
+    if result == 'hello world':
+        print(f'{count} attempts until the result')
+        break
