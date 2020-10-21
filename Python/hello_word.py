b76e2bdc8a938ef49168b3c7343781dba9424191
maurxeugenio <maurxeugenio@gmail.com>
2020-10-21 11:07:11 -0300

    I forgot to add +1 to the counter

--- Python/hello_word.py
+++ Python/hello_word.py
@@ -3,6 +3,7 @@ hw = ['o', 'l', 'w', 'l', 'e', 'h', 'l', 'r', 'd']
 
 count = 0
 while True:
+    count += 1
     result = f'{hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]} {hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]}{hw[randint(0, 8)]}'
     print(result)
     if result == 'hello world':
