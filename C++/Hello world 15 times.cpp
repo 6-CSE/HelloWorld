a8208f88a4d55f62b4e2e00a4066eeef61e04759
Rishit Saiya <180010027@iitdh.ac.in>
2020-10-26 16:05:23 +0530

    Update Hello world 15 times.cpp

--- C++/Hello world 15 times.cpp
+++ C++/Hello world 15 times.cpp
@@ -1,13 +1,9 @@
-
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	for (int hw = 1; hw <= 15; hw=hw+1)
-	{
+int main () {
+	for (int hw = 1; hw <= 15; hw++) {
 		cout << "Hello-World\n";
 	}
-
-	return 0;
+  return 0;
 }
