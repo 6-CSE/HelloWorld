8753ab894d1326d46137e3203808ca7f1623f1c9
Hellsgate96 <50798361+Hellsgate96@users.noreply.github.com>
2021-10-05 21:20:06 +0530

    Create helloWorld.cs

---
+++ C#/helloWorld.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Installation {
+    class MainClass {
+        public static void Main (string[] args) {
+            Console.WriteLine ("Hello World !");
+        }
+    }
+}
