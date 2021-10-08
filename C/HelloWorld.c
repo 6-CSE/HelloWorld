8d04db46167cb14e1e3e4c4b56262fd93074d2da
Chamudi Rasanjalee <91789995+chamz99@users.noreply.github.com>
2021-10-08 19:28:24 +0530

    Code convert as decimal to binary convertor

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -2,5 +2,19 @@
 
 int main(){
     printf("Hello World!\n");
+    int number,i,j;
+    printf("\n\nEnter the integer number in decimal number system\n");
+    scanf("%d",&number);
+    
+    printf("%d in binary number is : \n",number);
+    for (i=31;i>=0;i--)
+    {
+        j = number >> i;
+        if (j & 1)
+            printf("1");
+        else
+            printf("0");
+    }
+    printf("\n");
     return 0;
 }
