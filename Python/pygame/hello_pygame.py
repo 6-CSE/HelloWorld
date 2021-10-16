51728dcd64ab63cc7561ab020367586ca0865455
tarn.ptmtp@gmail.com <tarn.ptmtp@gmail.com>
2021-10-16 17:04:41 +0700

    Add heelo pygame

---
+++ Python/pygame/hello_pygame.py
@@ -0,0 +1,36 @@
+import subprocess
+import sys
+
+# check exists package
+try:
+    import pygame
+    import pygame_menu
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pygame==2.0.2"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pygame-menu==4.1.4"])
+    import pygame
+    import pygame_menu
+
+# init pygame
+pygame.init()
+surface = pygame.display.set_mode((600, 400))
+
+# make your menu
+def set_difficulty(value, difficulty):
+    # Do the job here !
+    pass
+
+def start_the_game():
+    # Do the job here !
+    pass
+
+menu = pygame_menu.Menu('Welcome', 400, 300,
+                       theme=pygame_menu.themes.THEME_BLUE)
+
+menu.add.text_input('Name :', default='John Doe')
+menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+# run game menu
+menu.mainloop(surface)
