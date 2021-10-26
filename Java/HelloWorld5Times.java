d9efb8da88b3fc962d67b6cc71b287fc1dabeb02
Fauzan Pradana <72968983+fauzanpr@users.noreply.github.com>
2021-10-26 17:00:40 +0700

    Create HelloWorld5Times.java

---
+++ Java/HelloWorld5Times.java
@@ -0,0 +1,11 @@
+class HelloWorld5Times {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World");
+        }
+
+    }
+
+}
