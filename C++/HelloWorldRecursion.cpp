54962c203a802cbc3baa70a48c0829869037b1e4
Rishit Saiya <180010027@iitdh.ac.in>
2022-09-30 14:56:34 -0700

    Updated with required changes

---
+++ C++/HelloWorldRecursion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+std::string hello = "Hello World";
+
+void print_hello(std::string s,int length,int index){
+   
+   if(index == length){
+      std::cout << std::endl;
+      return;
+   }
+   
+   std::cout<<s[index];
+   print_hello(s, length, index+1);
+}
+
+
+int main(){
+
+    print_hello(hello, hello.length(), 0);
+    return 0;
+}
