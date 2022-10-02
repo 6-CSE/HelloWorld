1ed3e336db95656d9d56293cf6517c96ac223691
adielliot37 <98006758+adielliot37@users.noreply.github.com>
2022-10-02 11:23:25 +0530

    Update HelloWorld.sol

--- Solidity/HelloWorld.sol
+++ Solidity/HelloWorld.sol
@@ -1,11 +1,11 @@
-//SPDX-License-Identifier: NoLicense
-// compiler version must be greater than or equal to 0.8.3 and less than 0.9.0
-pragma solidity ^0.8.0;
+// LANGUAGE: solidity
+// ENV: Sol
+// AUTHOR: Aditya Chaplot
+// GITHUB: https://github.com/adielliot37
+
+
+pragma solidity ^0.8.13;
 
 contract HelloWorld {
-    string hello = "Hello World!";
-    
-    function getHello() public returns (string memory){
-        return hello;
-    }
+    string public greet = "Hello World!";
 }
