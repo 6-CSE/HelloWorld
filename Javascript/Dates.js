432e752ec0bd5c6aaabb59f09e6950308916dab1
nobuyaki <dimas2214sa@gmail.com>
2022-10-03 05:34:07 +0700

    Create Dates.js

---
+++ Javascript/Dates.js
@@ -0,0 +1,19 @@
+const date = new Date()
+
+// Print Date in Complete Form
+console.log(date)
+
+// Everything related to Date
+console.log("The Date is ", date.getDate())
+console.log("The Day is ", date.getDay())
+console.log("The Current Month is ", date.getMonth())
+console.log("The Year is ", date.getFullYear())
+
+// Everything related to Time
+console.log("The Current Time is ", date.getTime()) // In Milliseconds
+console.log("The Current Hour is ", date.getHours()) // In 24 Hour Format
+console.log("The Current Minute is ", date.getMinutes())
+console.log("The Current Second is ", date.getSeconds())
+console.log("The Current Millisecond is ", date.getMilliseconds())
+
+// Similar are Javascript Set Methods
