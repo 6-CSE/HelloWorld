7b5417774f941f586d3aef2ba2ecf5a47a9f9854
Asfand Ali <22sw041@students.muet.edu.pk>
2023-10-05 11:28:46 +0500

    add loop run 10 time Hello World Java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		for (int i = 1; i <=10; i++) {
+			System.out.println("Hello World from Java");
+		}
 	}
 }
